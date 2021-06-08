56ac5a7901c38169f91d773aaa9c9547f093c846
Harrry1314 <54911666+Harrry1314@users.noreply.github.com>
2021-06-08 18:32:43 -0400

    write and use method calculate_grade
    Question 2.3

--- assignment02/grades/lib/grades/calculator.ex
+++ assignment02/grades/lib/grades/calculator.ex
@@ -4,7 +4,7 @@ defmodule Grades.Calculator do
 
     avg_labs = avg(labs)
 
-    mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+    mark = calculate_grade(avg_labs, avg_homework, midterm, final)
     round(mark * 100)
   end
 
@@ -23,7 +23,7 @@ defmodule Grades.Calculator do
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       "EIN"
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> "A+"
@@ -56,7 +56,7 @@ defmodule Grades.Calculator do
     if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> 10
@@ -86,5 +86,8 @@ defmodule Grades.Calculator do
 	else
 		false
 	end
+  end
+  def calculate_grade(avg_labs, avg_homework, midterm, final) do
+	0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
   end
 end
