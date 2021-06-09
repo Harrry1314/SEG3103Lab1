09c86f0df37d512b5aea9c344983e95a5e7cbb9b
Harrry1314 <54911666+Harrry1314@users.noreply.github.com>
2021-06-09 18:15:52 -0400

    refactor the code and the tests still pass

--- lab04/simpleCalculator/src/simpleCalculator/Calculator.java
+++ lab04/simpleCalculator/src/simpleCalculator/Calculator.java
@@ -3,28 +3,43 @@ package simpleCalculator;
 public class Calculator
 {
 
-	public int add(int a, int b)
+	public Object add(int a, int b)
 	{
-		return a+b;
+		return calculate('A',a,b);
 	}
-	public int minus(int a, int b)
+	public Object minus(int a, int b)
 	{
-		return a-b;
+		return calculate('M',a,b);
 	}
-	public int times(int a, int b)
+	public Object times(int a, int b)
 	{
-		return (a*b);
+		return calculate('T',a,b);
 	}
 	public Object divide(int a, int b)
 	{
-		if(b==0)
+		return calculate('D',a,b);
+	}
+	public Object calculate(char c, int a, int b)
+	{
+		switch(c)
 		{
-			return "Illegal Arguments";
-		}
-		else
-		{
-			return a/b;
+			case 'A':
+				return a+b;
+			case 'M':
+				return a-b;
+			case 'T':
+				return a*b;
+			case 'D':
+				if(b==0)
+				{
+					return "Illegal Arguments";
+				}
+				else
+				{
+					return a/b;
+				}
+			default:
+				return null;
 		}
 	}
-
 }
