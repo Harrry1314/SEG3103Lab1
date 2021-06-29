ae9a197dc8a37e4eca0ff980883090d9010a7a66
Harrry1314 <54911666+Harrry1314@users.noreply.github.com>
2021-06-29 01:12:49 -0400

    stubbed code

---
+++ lab05/grades/lib/grades/calculator.ex
@@ -0,0 +1,13 @@
+defmodule Grades.Calculator do
+  def percentage_grade(mark) do
+	Enum.random(0..100)
+  end
+
+  def letter_grade(mark) do
+	Enum.random(["A+","A","A-","B+","B","C+","C","D+","D","E","F","EIN"])
+  end
+
+  def numeric_grade(mark) do
+	Enum.random(1..10)
+  end
+end
